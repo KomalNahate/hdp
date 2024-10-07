b3a179364abcf032df0fb8ce4ab1bd3a2cdde693
KomalNahate <komalnahate91@gmail.com>
2024-10-07 19:03:48 +0000

    new notebook

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int mark[5]={99,100,77,44,88};
+
+    cout<<mark[0]<<endl;
+
+    cout<<mark[1]<<endl;
+
+    cout<<mark[2]<<endl;
+
+    cout<<mark[]3<<endl;
+    return 0;
+}
